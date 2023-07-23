229200364a87e16cd5c650f85b8e7ceb7c0c4a80
Shivansh Bhardwaj <96634814+bhardwajshivansh@users.noreply.github.com>
2023-07-23 17:46:11 +0530

    made basic files

---
+++ client/src/components/Layout/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-dark text-light'>
+      <h6 className='text_center'>All rights reserved &copy; budget_bounty</h6>
+    </div>
+  )
+}
+
+export default Footer
